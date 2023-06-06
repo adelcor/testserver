656064e351576c7b82c444967e775bc459751d55
adelcor <lgrnvsn@gmail.com>
2023-06-06 20:42:37 +0200

    all

---
+++ junio/server.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddress, clientAddress;
+//    char buffer[1024];
+    char c;
+
+    // Crear el socket
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Error al crear el socket." << std::endl;
+        return 1;
+    }
+
+    // Configurar la dirección del servidor
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(8080);
+
+    // Enlazar el socket al puerto
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Error al enlazar el socket al puerto." << std::endl;
+        close(serverSocket);
+        return 1;
+    }
+
+    // Escuchar conexiones entrantes
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "Error al escuchar conexiones entrantes." << std::endl;
+        close(serverSocket);
+        return 1;
+    }
+
+    std::cout << "Servidor en ejecución. Esperando conexiones..." << std::endl;
+	
+        socklen_t clientAddressLength = sizeof(clientAddress);
+
+        // Aceptar una conexión entrante
+        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+        if (clientSocket == -1) {
+            std::cerr << "Error al aceptar la conexión entrante." << std::endl;
+            close(serverSocket);
+            return 1;
+        }
+
+        // Leer la solicitud del cliente
+//        memset(buffer, 0, sizeof(buffer));
+	ssize_t bytesRead;
+	std::string data;
+	
+         while((bytesRead = recv(clientSocket, &c, 1, 0)) > 0)
+	 {
+        	if (bytesRead == -1) 
+		{
+            	std::cerr << "Error al leer la solicitud del cliente." << std::endl;
+            	close(clientSocket);
+            	close(serverSocket);
+            	return 1;
+        	}
+		else if(bytesRead == 0)
+		{
+			break;
+		}
+
+
+		std::cout << c << std::endl;
+		data.push_back(c);
+		
+	}
+
+        // Imprimir la solicitud en pantalla
+
+        // Cerrar la conexión con el cliente
+        close(clientSocket);
+
+	std::cout << data << std::endl;
+	
+
+    // Cerrar el socket del servidor
+    close(serverSocket);
+
+    return 0;
+}
+
